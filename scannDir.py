070e7911184cb89562ed5e9363ce1f05206875d2
Tobias Sigmann <tobias.sigmann@googlemail.com>
2018-09-17 00:09:32 +0200

    add function which ask the user what to do if a conflict accures

--- scannDir.py
+++ scannDir.py
@@ -11,7 +11,7 @@ import filecmp
 def checkSortDir(sortdir):
 	yearfolder = []
 	print("Checking "+sortdir)
-	for root, dirs, files in os.walk(sortdir):
+	for root, dirs, files in os.walk(sortdir):										#scann sort dir
 		for dir in dirs:
 			if re.match("[0-9]*", dir):
 				yearfolder.append(dir);
@@ -29,12 +29,12 @@ def checkSortDir(sortdir):
 def scannDirectory(scanndir, endings = "non"):
 	filesToSort = []
 	print("scanning " + scanndir)
-	for root, dirs, files in os.walk(scanndir):
+	for root, dirs, files in os.walk(scanndir):										#scann dir to sort
 		for file in files:
 			path = os.path.join(root, file)
-			if os. path. isfile(path):
+			if os.path.isfile(path):
 				filename, file_extension = os.path.splitext(path)
-				if endings == "non":
+				if endings == "non":												#filter by exrension
 					filesToSort.append(path)
 				elif endings == file_extension[1:]:
 					filesToSort.append(path)
@@ -48,6 +48,8 @@ def scannDirectory(scanndir, endings = "non"):
 #@param yearfolder	:array which contains existing year folderself.
 #@param filesToSort	:array which contains the files which have to be sorted.
 def sortFiles(sortdir, yearfolder, filesToSort):
+	originalErrorInFiles = []
+	sortedErrorInFiles = []
 	for file in filesToSort:
 		basename = os.path.basename(file)
 		(year, month, day, hours, minutes, seconds, wday, ydey, test)= time.localtime(os.stat(file)[8])	#calculate cration date
@@ -60,7 +62,8 @@ def sortFiles(sortdir, yearfolder, filesToSort):
 				if filecmp.cmp(file, fileNew):										#check if the files are the same
 					os.remove(file)
 				else:
-					print (file +"is differnet then "+fileNew)
+					originalErrorInFiles.append(file)
+					sortedErrorInFiles.append(fileNew)
 		else:
 			os.mkdir(os.path.join(sortdir,year))
 			yearfolder.append(year)
@@ -70,7 +73,44 @@ def sortFiles(sortdir, yearfolder, filesToSort):
 				if filecmp.cmp(file, fileNew):										#check if the files are the same
 					os.remove(file)
 				else:
-					print (file +"is differnet then "+fileNew)
+					originalErrorInFiles.append(file)
+					sortedErrorInFiles.append(fileNew)
+	return originalErrorInFiles, sortedErrorInFiles
+
+#function with ask the user which file should be ceaptself.
+#
+#@param originalErrorInFiles	: contains files in the to sort location
+#@param sortedErrorInFiles		: contains files in the sorted location
+def askWhatToDo(originalErrorInFiles, sortedErrorInFiles):
+	if originalErrorInFiles:
+		print(str(len(originalErrorInFiles)) + " conflicts")
+		print("0 - do nothing")
+		print("type in which file should be deleted")
+		for i in range(len(originalErrorInFiles)):
+			acceptInput = 1
+			while acceptInput:
+				print("-----------------------")
+				print("(1)"+originalErrorInFiles[i] + " (2)" + sortedErrorInFiles[i])
+				userInput = "error"
+				
+				try:
+					userInput = int(input("Do action: "))
+				except ValueError:
+					print("wrong input")
+
+				if userInput == 0:
+					acceptInput = 0
+				elif userInput == 1:
+					os.remove(originalErrorInFiles[i])
+					acceptInput = 0
+				elif userInput == 2:
+					os.remove(sortedErrorInFiles[i])
+					acceptInput = 0
+				else:
+					print("Not accepted input.")
+					print("0 - do nothing")
+					print("1 - delete original file ")
+					print("2 - delete files in sorted location")
 
 #The function takes two path and sorts the files in the first path and then
 #copy it to argument two. The files can be filtered by the endingsself.
@@ -81,11 +121,13 @@ def sortFiles(sortdir, yearfolder, filesToSort):
 def runSort(scanndir,sortdir,endings = "non"):
 	yearfolder = checkSortDir(sortdir)
 	filesToSort = scannDirectory(scanndir)
-	sortFiles(sortdir, yearfolder, filesToSort)
-
+	originalErrorInFiles, sortedErrorInFiles = sortFiles(sortdir, yearfolder, filesToSort)
+	askWhatToDo(originalErrorInFiles, sortedErrorInFiles)
 
 
 scanndir = "c:\\Users\\siggi\\Downloads\\"
 sortdir = "c:\\Users\\siggi\\Desktop\\sort\\"
 
 runSort(scanndir, sortdir)
+
+#TODO:add rename option
