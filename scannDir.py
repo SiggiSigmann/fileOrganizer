25ae89aeca28bbc1d241ac3380e61cbb1088cf38
Tobias Sigmann <tobias.sigmann@googlemail.com>
2018-09-16 08:29:27 +0200

    add comments to the functions, add endings option to scannDirectory

--- scannDir.py
+++ scannDir.py
@@ -2,6 +2,11 @@ import os
 import time
 import re
 
+#scanns the given directory and return all folders witch consists out of 4
+#digits as an arrayself.
+#
+#@param sortdir		:directory to be checked
+#@return			:array which contains the found folders
 def checkSortDir(sortdir):
 	yearfolder = []
 	print("Checking "+sortdir)
@@ -13,18 +18,35 @@ def checkSortDir(sortdir):
 	print(str(len(yearfolder)) + " folders was found.")
 	return yearfolder
 
-
-def scannDirectory(scanndir):
+#scanns the given directory and return all files in an array. If a second
+#parameter is givin it only adds the file to the array if the ending of the file
+#match the value of the argument "ending".
+#
+#@param scanndir	:contains path which has to be scanned.
+#@param endings		:(optional)ending of the file which shouls be returned.
+#@returned			:array which contaisn related fiels.
+def scannDirectory(scanndir, endings = "non"):
 	filesToSort = []
 	print("scanning " + scanndir)
 	for root, dirs, files in os.walk(scanndir):
 		for file in files:
 			path = os.path.join(root, file)
 			if os. path. isfile(path):
-				filesToSort.append(path)
+				filename, file_extension = os.path.splitext(path)
+				if endings == "non":
+					print("non")
+					filesToSort.append(path)
+				elif endings == file_extension[1:]:
+					filesToSort.append(path)
 	print(str(len(filesToSort)) + " files was found.")
 	return filesToSort
 
+#The subroutins move the files into the folder sortdir and sort them into the
+#subdirectory to sort them by yearself.
+#
+#@param sortdir		:path of the folder which will contain the sorted files.
+#@param yearfolder	:array which contains existing year folderself.
+#@param filesToSort	:array which contains the files which have to be sorted.
 def sortFiles(sortdir, yearfolder, filesToSort):
 	for file in filesToSort:
 		basename = os.path.basename(file)
@@ -32,7 +54,8 @@ def sortFiles(sortdir, yearfolder, filesToSort):
 		year = str(year)
 		if year in yearfolder:
 			print(os.path.join(sortdir,basename))
-			os.rename(file,os.path.join(os.path.join(sortdir,year),basename))
+			#os.rename(file,os.path.join(os.path.join(sortdir,year),basename))
+			print(os.path.join(os.path.join(sortdir,year),basename))
 		else:
 			os.mkdir(os.path.join(sortdir,year))
 			yearfolder.append(year)
